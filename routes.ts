354b6251370e24f12d7c405794c2eaebf0726447
Rémi Raumain <remiraumain@gmail.com>
2024-02-03 14:14:12 +0100

    Update bcrypt to bcryptjs and fix login functionality

---
+++ routes.ts
@@ -0,0 +1,29 @@
+/**
+ * An array of public routes.
+ * @type {string[]}
+ */
+
+export const publicRoutes = ["/"];
+
+/**
+ * An array of auth routes.
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after logging in
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
