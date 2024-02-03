f7bb5586083da39b946453a0c7d37dc0699b9c95
Rémi Raumain <remiraumain@gmail.com>
2024-02-03 15:00:47 +0100

    Add session and jwt callbacks to handle user roles

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import { UserRole } from "@prisma/client";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +12,25 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      console.log({ sessionToken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const exestingUser = await getUserById(token.sub);
+      if (!exestingUser) return token;
+      token.role = exestingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
